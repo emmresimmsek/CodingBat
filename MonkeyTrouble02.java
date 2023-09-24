7285e7b5ddd0e563f3fee0119d725e7528809103
Emre Şimşek <121165489+emmresimmsek@users.noreply.github.com>
2023-09-24 18:04:56 +0300

    Add files via upload

---
+++ MonkeyTrouble02.java
@@ -0,0 +1,26 @@
+public class MonkeyTrouble02 {
+    public static void main(String[] args) {
+    /* We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling.
+    We are in trouble if they are both smiling or if neither of them is smiling.
+    Return true if we are in trouble.
+    */
+
+        System.out.println(monkeyTrouble(true,true));
+
+    }
+
+    public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+       return aSmile == bSmile;  // KISA YOL!
+
+    }
+
+
+
+
+
+
+    }
+
+
+
