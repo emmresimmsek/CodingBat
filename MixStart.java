536c40f20d3d883125e60ed5d9dfea06f212bab2
Emre Şimşek <121165489+emmresimmsek@users.noreply.github.com>
2023-10-03 16:28:31 +0300

    Add files via upload

---
+++ MixStart.java
@@ -0,0 +1,66 @@
+
+public class MixStart {
+    public static void main(String[] args) {
+
+        /*
+
+      Return true if the given string begins with "mix", except the 'm' can be anything, so "pix", "9ix" .. all count.
+
+
+            mixStart("mix snacks") → true
+            mixStart("pix snacks") → true
+            mixStart("piz snacks") → false
+
+      */
+
+
+        System.out.println(mixStart("nix"));
+    }
+
+    public static boolean mixStart(String str) {
+        if (str.length()  >=  3) {
+        return  str.substring(1,3).equals("ix")  ;
+
+        } else {
+
+            return false;
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
