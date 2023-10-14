cfc0de8ab7a0ad78b3a9e75c19cb6ef2f0b3dab5
Emre Şimşek <121165489+emmresimmsek@users.noreply.github.com>
2023-10-14 15:02:57 +0300

    Add files via upload

---
+++ StringTimes.java
@@ -0,0 +1,36 @@
+public class StringTimes {
+    public static void main(String[] args) {
+
+        /*  Given a string and a non-negative int n, return a larger string that is
+        n copies of the original string.
+
+
+        stringTimes("Hi", 2) → "HiHi"
+        stringTimes("Hi", 3) → "HiHiHi"
+        stringTimes("Hi", 1) → "Hi"
+
+       */
+
+        System.out.println(stringTimes("Hi",3));
+
+    }
+
+
+    public static String stringTimes(String str, int n) {
+
+        String result = "";
+
+        for (int i = 0; i < n ; i++) {
+
+            result += str;
+
+
+
+        }   return  result   ;
+
+
+
+
+    }
+
+}
